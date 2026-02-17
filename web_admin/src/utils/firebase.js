817b5f06d6d1c7819d37cdae7bbefbd0758fdf4d
barofarsamo <142121817+barofarsamo@users.noreply.github.com>
2026-02-17 11:50:34 +0300

    Update Firebase configuration with new credentials

--- web_admin/src/utils/firebase.js
+++ web_admin/src/utils/firebase.js
@@ -1,14 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCMDafOlxq2pDDAR_NmKUn-LBpUNmc8Uho",
+  authDomain: "riobox-73d08.firebaseapp.com",
+  projectId: "riobox-73d08",
+  storageBucket: "riobox-73d08.firebasestorage.app",
+  messagingSenderId: "243412248404",
+  appId: "1:243412248404:web:69f26829cf8cc6137f4ac1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
